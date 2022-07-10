2fb81433180bb87aa061f4cb15b4b048d451aaf1
jbarbieri1990 <54899980+jbarbieri1990@users.noreply.github.com>
2022-07-10 00:58:37 -0400

    Challeng 01 - Original

--- Revit_Addin_Academy/Command01Challenge.cs
+++ Revit_Addin_Academy/Command01Challenge.cs
@@ -26,40 +26,41 @@ namespace Revit_Addin_Academy
 			Document doc = uidoc.Document;
 
 			//string text = "Revit Add-In Academy";
-			
+
 			// Variables for Session 01 Challenge
 
-				string filename = doc.PathName;
-				string output1 = "Fizz";
-				string output2 = "Buzz";
-				int range = 100;
-				double offset = 0.05;
-				double offsetCalc = offset * doc.ActiveView.Scale;
+			string filename = doc.PathName;
+			string output1 = "Fizz";
+			string output2 = "Buzz";
+			int range = 100;
+			double offset = 0.05;
+			double offsetCalc = offset * doc.ActiveView.Scale;
 
 			// Variables to offset text notes
 
-				XYZ curPoint = new XYZ(0, 0, 0);
-				XYZ offsetPoint = new XYZ(0, offsetCalc, 0);
+			XYZ curPoint = new XYZ(0, 0, 0);
+			XYZ offsetPoint = new XYZ(0, offsetCalc, 0);
 
 			// Collect the first text note to use below
-				FilteredElementCollector collector = new FilteredElementCollector(doc);
-				collector.OfClass(typeof(TextNoteType));
+			FilteredElementCollector collector = new FilteredElementCollector(doc);
+			collector.OfClass(typeof(TextNoteType));
 
 
 			//Begin Transaction on Revit Model
-				Transaction t = new Transaction(doc, "Create Text Note");
-					t.Start();
-					
+			Transaction t = new Transaction(doc, "Create Text Note");
+			t.Start();
+
 
 			// For Loop to see if number is divisible by 3 &/or 5 or both, if not print number to text
-				for (int i = 1; i <= range; i++)
-				{
-					if (i % 3 == 0 && i % 5 == 0)
+		
+			for (int i = 1; i <= range; i++)
+			{
+				if (i % 3 == 0 && i % 5 == 0)
 				{
 					TextNote curNote = TextNote.Create(doc, doc.ActiveView.Id, curPoint, output1 + output2, collector.FirstElementId());
 					curPoint = curPoint.Subtract(offsetPoint);
 				}
-				if (i % 3 == 0) 
+				if (i % 3 == 0)
 				{
 					TextNote curNote = TextNote.Create(doc, doc.ActiveView.Id, curPoint, output1, collector.FirstElementId());
 					curPoint = curPoint.Subtract(offsetPoint);
@@ -69,16 +70,16 @@ namespace Revit_Addin_Academy
 					TextNote curNote = TextNote.Create(doc, doc.ActiveView.Id, curPoint, output2, collector.FirstElementId());
 					curPoint = curPoint.Subtract(offsetPoint);
 				}
-				else 
+				else
 				{
 					TextNote curNote = TextNote.Create(doc, doc.ActiveView.Id, curPoint, i.ToString(), collector.FirstElementId());
 					curPoint = curPoint.Subtract(offsetPoint);
 				}
-				}
-												
+			}
+
 			// End Transaction
-				t.Commit();
-				t.Dispose();
+			t.Commit();
+			t.Dispose();
 
 			return Result.Succeeded;
 		}
